5d32841183db401e4d19857bd87e3aff7e566c9a
Eduarda <eduardadaminelli314@gmail.com>
2023-09-01 09:32:49 -0300

    verção com duas variaveis

--- calculadora.js
+++ calculadora.js
@@ -1,9 +1,12 @@
+var mensagen = require("./modulos/mensagem")
+var nome = "eduarda";
 var somar = require("./modulos/somar");
 var media = require("./modulos/media");
 var dividir = require("./modulos/dividir");
 var multiplicar = require("./modulos/multiplicar");
 
-console.log("A soma de X e Y é: "+somar(20,90));
-console.log(media(20,90));
-console.log(dividir(200,90));
-console.log(multiplicar(20,90));
+console.log(mensagen(nome));
+console.log("A soma de X e Y é: "+somar(20,80));
+console.log("A media de X e Y é: "+media(20,120));
+console.log("a divisão de X e Y é: "+dividir(80,20));
+console.log("multiplicação de X e Y é: "+multiplicar(20,5));
---
+++ modulos/mensagem.js
@@ -0,0 +1,5 @@
+var mensagem= function (nome,mensagem) {
+    return "ola, " + nome + " seja bem vinda a sua calculadora"; 
+
+  }; 
+  module.exports = mensagem; 
